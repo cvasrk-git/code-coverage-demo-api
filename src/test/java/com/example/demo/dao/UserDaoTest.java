05e1756b649d7a89234f5ebcf126a4ca41bce1a7
Krishna <serripothula@questk2.com>
2025-10-07 15:37:13 +0530

    ReflectionTestUtils.invokeMethod changes

--- src/test/java/com/example/demo/dao/UserDaoTest.java
+++ src/test/java/com/example/demo/dao/UserDaoTest.java
@@ -1,15 +1,9 @@
 package com.example.demo.dao;
 
-import com.example.demo.dao.UserDao;
 import com.example.demo.data.User;
-import com.example.demo.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
